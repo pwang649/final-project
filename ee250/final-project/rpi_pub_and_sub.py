ae25125de4a433e27a1bd577b98c97bdbb222c08
Peter Wang <pwang649@usc.edu>
2021-12-03 18:19:23 -0800

    more test

--- ee250/final-project/rpi_pub_and_sub.py
+++ ee250/final-project/rpi_pub_and_sub.py
@@ -90,13 +90,25 @@ if __name__ == '__main__':
     client.on_connect = on_connect
     client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
     client.loop_start()
-
+    digitalWrite(ledR,1)
+    digitalWrite(ledG,1)
+    digitalWrite(ledB,1)
     while True:
-        digitalWrite(ledR,1)
-        digitalWrite(ledG,1)
-        digitalWrite(ledB,1)
         soundValue = analogRead(soundSensor)
         print(soundValue)
         # publish the ultrasonic reading
         client.publish("MONIPET/soundSensor", soundValue)
+        if not manual_control_mode:
+            if soundValue > 400:
+                digitalWrite(ledR,1)
+                digitalWrite(ledG,0)
+                digitalWrite(ledB,0)
+            elif soundValue > 200:
+                digitalWrite(ledR,0)
+                digitalWrite(ledG,0)
+                digitalWrite(ledB,1):
+            else:
+                digitalWrite(ledR,o)
+                digitalWrite(ledG,1)
+                digitalWrite(ledB,0)
         time.sleep(1)
--- ee250/final-project/vm_subscriber.py
+++ ee250/final-project/vm_subscriber.py
@@ -8,20 +8,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-
-    return render_template("index.html")
-    while True:
-        #print("delete this line")
-        time.sleep(1)
+    global soundValue
+    return render_template("index.html", sound = soundValue)
 
 @app.route('/submit', methods=['POST', 'GET'])
 def submit():
     if request.method == "POST":
+        if 'manual' in request.form.keys():
+            client.publish("MONIPET/manual", "true")
+        else:
+            client.publish("MONIPET/manual", "false")
         print(request.form)
         r = request.form["red"]
         b = request.form["blue"]
         g = request.form["green"]
-        client.publish("MONIPET/manual", "true")
         client.publish("MONIPET/ledR", r)
         client.publish("MONIPET/ledG", g)
         client.publish("MONIPET/ledB", b)
@@ -31,15 +31,13 @@ def submit():
     
 def custom_callback(client, userdata, message):
     # prints the ultrasonic ranger value 
-    print("VM: " + str(message.payload, "utf-8") + " cm")
+    print("VM: " + str(message.payload, "utf-8"))
+    global soundValue
+    soundValue = int(str(message.payload, "utf-8"))
    
-def button_callback(client, userdata, message):
-    m = str(message.payload, "utf-8")
-    print(str(m))
     
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
-    
     #subscribe to interested topics here
     client.subscribe("MONIPET/soundSensor")
     client.message_callback_add("MONIPET/soundSensor", custom_callback)
@@ -51,5 +49,6 @@ if __name__ == "__main__":
     client.on_connect = on_connect
     client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
     client.loop_start()
+    soundValue = 0
     app.secret_key = os.urandom(12)
     app.run(debug=True)
